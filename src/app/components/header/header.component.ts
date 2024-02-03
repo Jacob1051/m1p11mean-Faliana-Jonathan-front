e053cf1ec0f07f5354634d0f308503017137c621
Faliana <faliana23@gmail.com>
2024-02-03 16:24:23 +0300

    header information

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/client/auth/auth.service';
 import { UserInformationService } from 'src/app/services/user/userInformation/user-information.service';
 
 @Component({
@@ -10,33 +11,40 @@ import { UserInformationService } from 'src/app/services/user/userInformation/us
 export class HeaderComponent {
     constructor(
         private router: Router,
-        private service: UserInformationService
+        private service: UserInformationService,
+        private authService: AuthService
     ) {
-        this.user ? this.getUserInformation() : null;
     }
 
     isLoading: boolean = false;
-    user = localStorage.getItem('user')
-        ? JSON.parse(localStorage.getItem('user')!)
-        : null;
-    userInformation: any;
+    user: any;
+    userInformation:any;
+
+    ngOnInit(): void {
+      this.authService.user.subscribe((nouvelUtilisateur) => {
+        this.user = nouvelUtilisateur;
+        this.userInformation = this.user ? this.getUserInformation() : null;
+      });
+    }
 
     goToLogin() {
         this.router.navigateByUrl('/login');
     }
 
     getUserInformation() {
+        // console.log("eto");
         this.isLoading = true;
         this.service
             .getUserInformation(this.user.user_id, this.user.role)
             ?.subscribe({
                 next: (response: any) => {
+                    // console.log("eto2");
                     this.userInformation = response.data;
                     this.isLoading = false;
                     // console.log("donc tonga eto?");
                 },
                 error: (error) => {
-                    console.log("erreur", error);
+                    console.log('erreur', error);
                     this.isLoading = false;
                 },
             });
