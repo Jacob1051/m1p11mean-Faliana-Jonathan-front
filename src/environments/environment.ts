8737250d456fbb1951f3552ffd1b914fe90f8a23
Jonathan <stevenjacob906@gmail.com>
2024-02-13 09:54:40 +0300

    production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:4000'
+    // apiUrl: 'http://localhost:4000',
+    apiUrl: 'https://m1p11mean-jonathan-faliana-back.onrender.com'
 };
