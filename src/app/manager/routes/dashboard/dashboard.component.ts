e0e7c9259c4e82bcd24ac1174ffb2b628bb3565b
Jonathan <stevenjacob906@gmail.com>
2024-02-22 19:03:16 +0300

    question farany

--- src/app/manager/routes/dashboard/dashboard.component.ts
+++ src/app/manager/routes/dashboard/dashboard.component.ts
@@ -4,6 +4,8 @@ import { EmployeService } from 'src/app/shared/services/employe/employe.service'
 import { Employe } from 'src/app/shared/models/employe';
 import { firstValueFrom } from 'rxjs';
 import { Item } from 'src/app/shared/models/multi-dropdown';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import * as _ from 'lodash';
 
 @Component({
     selector: 'app-dashboard',
@@ -14,7 +16,22 @@ export class DashboardComponent implements OnInit{
     listeEmploye: Employe[] = [];
     listeEmployeAsItem: Item[] = [];
     choix: Item[] = [{id: 0, name:'Par Mois'}, {id: 1, name:'Par Jouir'}];
+    beneficeForm!: FormGroup;
 
+    monthsArray = [
+        { month: 0, name: "Janvier" },
+        { month: 1, name: "Fevrier" },
+        { month: 2, name: "Mars" },
+        { month: 3, name: "Avril" },
+        { month: 4, name: "Mai" },
+        { month: 5, name: "Juin" },
+        { month: 6, name: "Juillet" },
+        { month: 7, name: "Aout" },
+        { month: 8, name: "Septembre" },
+        { month: 9, name: "Octobre" },
+        { month: 10, name: "Novembre" },
+        { month: 11, name: "Decembre" }
+    ];
 
     //---------------------loading variable---------------------//
     isLoadingTempsMoyen = true;
@@ -37,15 +54,31 @@ export class DashboardComponent implements OnInit{
     revenue: any = [];
     //---------------------revenue variable---------------------//
 
+    //---------------------benefice variable---------------------//
+    benefice: any = [];
+    beneficeBackUp: any = [];
+    //---------------------benefice variable---------------------//
+
     constructor(
         private statService: StatsService,
         private employeService: EmployeService
     ){}
 
     async ngOnInit() {
+        this.beneficeForm = new FormGroup(
+            {
+                salary: new FormControl<Number>(0, [Validators.required]),
+                rent: new FormControl<Number>(0, [Validators.required]),
+                pieces: new FormControl<Number>(0, [Validators.required]),
+                other: new FormControl<Number>(0, [Validators.required]),
+                month: new FormControl(null, [Validators.required]),
+            }
+        );
+
         await this.getListeEmploye();
         this.getRdvReservation();
         this.getChiffreAffaire();
+        this.getBeneficeParMois();
     }
 
     async getListeEmploye() {
@@ -104,6 +137,19 @@ export class DashboardComponent implements OnInit{
         this.isLoadingRevenue = true;
     }
 
+    getBeneficeParMois(): void {
+        this.isLoadingRevenue = true;
+        this.statService.getChiffreAffaireParMois()
+            .subscribe({
+                next: (data: any) => {
+                    this.benefice = _.cloneDeep(data.data);
+                    this.beneficeBackUp = _.cloneDeep(data.data);
+                    this.isLoadingRevenue = false;
+                }
+            })
+        this.isLoadingRevenue = true;
+    }
+
     getChiffreAffaireParJour(): void {
         this.isLoadingRevenue = true;
         this.statService.getChiffreAffaireParJour()
@@ -155,5 +201,23 @@ export class DashboardComponent implements OnInit{
     OnChoiceItemRevenueChange(item: Item){
         this.selectedChoiceRevenue = item;
         this.getChiffreAffaire();
+    }
+
+    onSubmitBeneficeForm() {
+        const beneficeFormValue = this.beneficeForm.value;
+
+        this.benefice[1].value = this.benefice[1].value - (
+            beneficeFormValue.salary + beneficeFormValue.rent + beneficeFormValue.pieces + beneficeFormValue.other
+        );
+        this.benefice = [...this.benefice];
+
+    }
+
+    resetBenefice() {
+        this.benefice = _.cloneDeep(this.beneficeBackUp);
+    }
+
+    testChange(event: any){
+        console.log(event);
     }
 }
